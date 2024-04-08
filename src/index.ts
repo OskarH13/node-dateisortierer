48dac11e5252cff6ebac42871b548cf382d3f00f
OskarH13 <161576666+OskarH13@users.noreply.github.com>
2024-04-08 11:37:23 +0200

    Adding monday

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import * as fs from 'node:fs'
 import * as path from 'node:path'
-import { dateiEndungenErstellen } from './helpers'
+import { extractFileExtensions } from './helpers'
+import { createDirs } from './helpers'
 
+// Step 1. List the contents of the folder.
 
-// 1. Schritt: Inhalte des Ordners auflisten
-const inhalteOrnder = fs.readdirSync('./input')
-// console.log(inhalteOrnder)
+const dirContent = fs.readdirSync('./input')
+console.log(dirContent)
 
-// 2. Schritt: Dateitypen herausfinden
+// Step 2. Identufy file types.
 
-const endungen = dateiEndungenErstellen(inhalteOrnder)
-console.log(endungen)
+const extensions = extractFileExtensions(dirContent)
+console.log(extensions)
 
-// 3. Schritt: Dateien nach Bedingungen sortieren
+const extensionsArray = Array.from(extensions)
+console.log(extensionsArray)
+// Step 3. Sort files based on conditions.
+
+createDirs('./output', extensionsArray)
+
+//fs.copyFileSync('./input/', './output/.pdf/document.pdf')
+
+for (const file of dirContent) {
+    const ext = path.extname(file)
+    try {
+        fs.copyFileSync('./input/' + file, './output/' + ext + '/' + file)
+    } catch (e) {
+        console.error(e)
+    }
+}
